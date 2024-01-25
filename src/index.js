3aceb5dab1d5d8dd1d2eb4f037ac826fb073dad2
DanitzaVasquez <danitzavazquez991@gmail.com>
2024-01-24 20:12:21 -0700

    feature/01

--- src/index.js
+++ src/index.js
@@ -11,6 +11,11 @@ let tasks = [
   { id: 2, title: 'Task 2', description: 'Do something else' },
 ];
 
+// Get all issue
+app.get('/issues', (req, res) => {
+  res.json(tasks);
+});
+
 // Get all tasks
 app.get('/tasks', (req, res) => {
   res.json(tasks);
